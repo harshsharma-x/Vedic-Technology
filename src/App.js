a3f7074517559ea2c7c751b46cc712e23a18e0bf
abhinavbgpgit <abhinavbgp@gmail.com>
2024-06-20 01:47:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './my_components/Header';
+import {Routes,Route} from 'react-router-dom';
+import Pricing from './pages/Pricing';
+import Layout from './Layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Layout/>}/>
+        <Route path="/pricing" element={<Pricing/>}/>
+      </Routes>
     </div>
   );
 }
